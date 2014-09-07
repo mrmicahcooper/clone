b6d1f1acfebf4ff74f8a75b18f1f0a953d04ea41
Micah Cooper <mrmicahcooper+mrmicahcooper@gmail.com>
2014-09-07 13:18:15 -0400

    Setup config

--- config.rb
+++ config.rb
@@ -2,31 +2,13 @@ configure :development do
   activate :livereload
 end
 
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
-
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
 
-# Build-specific configuration
+set :haml, { format: :html5 }
+
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+  activate :minify_css
+  activate :minify_javascript
 end
